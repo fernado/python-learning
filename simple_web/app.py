07d5097e50cdb64f6d25ce9af1e35aca3ac48b0f
fernado <fernado@gmail.com>
2024-08-10 11:07:14 +0800

    update

--- simple_web/app.py
+++ simple_web/app.py
@@ -12,17 +12,30 @@ template_str = """
         {% endif %}
     {% endfor %}
 {% endif %}
+<h3>==========</h3>
+{% if no_check_result %}
+    {% for item in no_check_result %}
+        {{ item }}
+        {% if not loop.last %}
+            ,
+        {% endif %}
+    {% endfor %}
+{% endif %}
 """
 # @app.route('/')
 # def index():
 #     yes_check_result = ['item1', 'item2', 'item3']
-#     return render_template('index.html', yes_check_result=yes_check_result)
+#     # return render_template('index.html', yes_check_result=yes_check_result)
+#     return render_template_string(index.html, yes_check_result=yes_check_result, no_check_result=no_check_result)
+
 
 
 @app.route('/')
 def index():
     yes_check_result = ['item4', 'item5', 'item6']
-    return render_template_string(template_str, yes_check_result=yes_check_result)
+    no_check_result = ['item11', 'item12', 'item13', 'item14', 'item15']
+    #return render_template_string(template_str, yes_check_result=yes_check_result)
+    return render_template_string(template_str, yes_check_result=yes_check_result, no_check_result=no_check_result)
 
 if __name__ == '__main__':
     app.run()
